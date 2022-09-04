42f8d9f794b4362b983d19b81c0d59ce308b75be
LeoKu <czc12580520@gmail.com>
2022-09-05 00:09:39 +0800

    Green Wall

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -5,27 +6,33 @@ import GitHubButton from './GitHubButton'
 
 export default function Layout(props: { children?: React.ReactNode }) {
   return (
-    <div className="min-h-screen px-5 md:mx-auto md:min-w-content md:max-w-content lg:px-0">
-      <header>
-        <div className="flex h-header items-center">
-          <Link href="/">
-            <span className="flex cursor-pointer select-none items-center text-xl font-bold">
-              <span className="pointer-events-none w-9 md:w-7">
-                <Image height="100%" layout="responsive" src="/favicon.svg" width="100%" />
+    <>
+      <Head>
+        <title>Green Wall · Review your GitHub contributions</title>
+      </Head>
+
+      <div className="min-h-screen px-5 md:mx-auto md:min-w-content md:max-w-content lg:px-0">
+        <header>
+          <div className="flex h-header items-center">
+            <Link href="/">
+              <span className="flex cursor-pointer select-none items-center text-xl font-bold">
+                <span className="pointer-events-none w-9 md:w-7">
+                  <Image height="100%" layout="responsive" src="/favicon.svg" width="100%" />
+                </span>
+                <span className="ml-3 hidden md:inline">Green Wall</span>
               </span>
-              <span className="ml-3 hidden md:inline">Green Wall</span>
-            </span>
-          </Link>
+            </Link>
 
-          <GitHubButton />
-        </div>
-      </header>
+            <GitHubButton />
+          </div>
+        </header>
 
-      <main>{props.children}</main>
+        <main>{props.children}</main>
 
-      <footer className="sticky top-[100vh] py-3 text-center text-sm text-main-400">
-        Power by Vercel
-      </footer>
-    </div>
+        <footer className="sticky top-[100vh] py-3 text-center text-sm text-main-400">
+          Power by Vercel
+        </footer>
+      </div>
+    </>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import splitbee from '@splitbee/web'
-import { toPng as toJpeg } from 'html-to-image'
-import Head from 'next/head'
+import { toPng } from 'html-to-image'
 import { type FormEventHandler, useEffect, useRef, useState } from 'react'
 
 import ContributionsGraph from '../components/ContributionsGraph'
@@ -72,7 +71,7 @@ export default function HomePage() {
         setDownloading(true)
         splitbee.track('Click Download')
 
-        const dataURL = await toJpeg(graphRef.current)
+        const dataURL = await toPng(graphRef.current)
         const trigger = document.createElement('a')
         trigger.href = dataURL
         trigger.download = `${graphData.username}`
@@ -95,94 +94,88 @@ export default function HomePage() {
   }, [graphData])
 
   return (
-    <>
-      <Head>
-        <title>Green Wall · Review your GitHub contributions</title>
-      </Head>
+    <div className="py-10 md:py-14">
+      <h1 className="text-center text-3xl font-bold md:mx-auto md:px-20 md:text-6xl md:leading-[1.2]">
+        Review the contributions you have made on GitHub over the years.
+      </h1>
 
-      <div className="py-10 md:py-14">
-        <h1 className="text-center text-3xl font-bold md:mx-auto md:px-20 md:text-6xl md:leading-[1.2]">
-          Review the contributions you have made on GitHub over the years.
-        </h1>
-
-        <form className="py-12 md:py-16" onSubmit={handleSubmit}>
-          <div className="flex flex-col items-center justify-center gap-y-6 md:flex-row md:gap-x-5">
-            <input
-              ref={inputRef}
-              required
-              className={`
+      <form className="py-12 md:py-16" onSubmit={handleSubmit}>
+        <div className="flex flex-col items-center justify-center gap-y-6 md:flex-row md:gap-x-5">
+          <input
+            ref={inputRef}
+            required
+            className={`
               inline-block h-[2.8rem] overflow-hidden rounded-lg bg-main-100 px-5
               text-center text-lg font-medium text-main-600 caret-main-500 shadow-main-300 outline-none
               transition-all duration-300
               placeholder:select-none placeholder:font-normal placeholder:text-main-400
               focus:bg-white focus:shadow-[0_0_1.5rem_var(--tw-shadow-color)]
               `}
-              name="username"
-              placeholder="GitHub Username"
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <GenerateButton loading={loading} type="submit" />
-          </div>
-        </form>
+            name="username"
+            placeholder="GitHub Username"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <GenerateButton loading={loading} type="submit" />
+        </div>
+      </form>
 
-        {error ? (
-          <ErrorMessage message={error.message} />
-        ) : (
-          <Loading active={loading}>
-            {graphData && (
-              <>
-                <div
-                  ref={actionRef}
-                  className="flex flex-row-reverse flex-wrap items-center justify-center gap-x-6 gap-y-4 py-5"
+      {error ? (
+        <ErrorMessage message={error.message} />
+      ) : (
+        <Loading active={loading}>
+          {graphData && (
+            <>
+              <div
+                ref={actionRef}
+                className="flex flex-row-reverse flex-wrap items-center justify-center gap-x-6 gap-y-4 py-5"
+              >
+                <button
+                  className={`inline-flex h-full items-center rounded-md bg-main-100 py-2 px-4 font-medium text-main-500 focus:outline-none disabled:pointer-events-none ${
+                    downloading ? 'animate-bounce' : ''
+                  }`}
+                  disabled={downloading}
+                  onClick={handleDownload}
                 >
-                  <button
-                    className={`inline-flex h-full items-center rounded-md bg-main-100 py-2 px-4 font-medium text-main-500 focus:outline-none disabled:pointer-events-none ${
-                      downloading ? 'animate-bounce' : ''
-                    }`}
-                    disabled={downloading}
-                    onClick={handleDownload}
-                  >
-                    {iconImage}
-                    <span className="ml-2">Save as Image</span>
-                  </button>
-                  <div className="flex items-center gap-x-6">
-                    <ShareButton theme={theme?.name} username={graphData.username} />
-                    <SettingButton
-                      content={
-                        <div className="text-main-400">
-                          <div className="flex items-center justify-between py-2">
-                            <label className="" htmlFor="origin">
-                              Origin
-                            </label>
-                            <Switch id="origin" />
-                          </div>
-                          <div>
-                            <label className="mb-2 inline-block font-bold">Theme</label>
-                            <ThemeSelector
-                              value={theme}
-                              onChange={(theme) => {
-                                splitbee.track('Change theme', { themeName: theme.name })
-                                setTheme(theme)
-                              }}
-                            />
-                          </div>
+                  {iconImage}
+                  <span className="ml-2">Save as Image</span>
+                </button>
+                <div className="flex items-center gap-x-6">
+                  <ShareButton theme={theme?.name} username={graphData.username} />
+                  <SettingButton
+                    content={
+                      <div className="text-main-400">
+                        <div className="flex items-center justify-between py-2">
+                          <label className="" htmlFor="origin">
+                            Origin
+                          </label>
+                          <Switch id="origin" />
                         </div>
-                      }
-                    />
-                  </div>
+                        <div>
+                          <label className="mb-2 inline-block font-bold">Theme</label>
+                          <ThemeSelector
+                            value={theme}
+                            onChange={(theme) => {
+                              splitbee.track('Change theme', { themeName: theme.name })
+                              setTheme(theme)
+                            }}
+                          />
+                        </div>
+                      </div>
+                    }
+                  />
                 </div>
+              </div>
 
-                <div className="flex overflow-x-auto md:justify-center">
-                  <ContributionsGraph ref={graphRef} data={graphData} theme={theme?.name} />
-                </div>
-              </>
-            )}
-          </Loading>
-        )}
-      </div>
-    </>
+              <div className="flex overflow-x-auto md:justify-center">
+                <ContributionsGraph ref={graphRef} data={graphData} theme={theme?.name} />
+              </div>
+            </>
+          )}
+        </Loading>
+      )}
+    </div>
   )
 }
 
