3fc9879a2e610d54f4bd47b15ad9a860d71ae4ad
LeoKu <czc12580520@gmail.com>
2022-10-06 14:32:19 +0800

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,15 +2,7 @@ module.exports = {
   root: true,
   extends: [require.resolve('prefer-code-style/lib/eslint')],
   rules: {
-    'react/react-in-jsx-scope': 0,
-    'jsx-a11y/no-static-element-interactions': 0,
-    'jsx-a11y/click-events-have-key-events': 0,
     '@typescript-eslint/no-explicit-any': 0,
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
   },
   ignorePatterns: [
     'public',
