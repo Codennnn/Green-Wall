16f0fbbecd4b8844312223a025796ffc7bee28cf
LeoKu <czc12580520@gmail.com>
2022-10-08 20:14:22 +0800

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,9 @@
 module.exports = {
   root: true,
   extends: [require.resolve('prefer-code-style/lib/eslint')],
-  rules: {
-    '@typescript-eslint/no-explicit-any': 0,
-  },
   ignorePatterns: [
     'public',
-    'es',
-    'dist*',
     'yarn*',
-    '.cache',
     '.next',
     '!.eslintrc.js',
     '!.prettierrc.js',
