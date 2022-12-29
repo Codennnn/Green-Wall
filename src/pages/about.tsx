82ba1fdd19bf3052b0686dd0f0ee2d61bda57907
LeoKu <czc12580520@gmail.com>
2022-12-29 16:18:19 +0800

    Update about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,11 +2,29 @@ import Link from 'next/link'
 
 import Layout from '../components/Layout'
 
+const SectionTitle = (props: React.PropsWithChildren) => (
+  <h2 className="mb-1 mt-5 text-lg font-bold first-of-type:mt-0 md:mt-8 md:mb-2 md:text-2xl">
+    {props.children}
+  </h2>
+)
+
+const TextLink = ({
+  children,
+  ...props
+}: React.PropsWithChildren<React.ComponentProps<typeof Link>>) => (
+  <Link
+    className="underline decoration-current decoration-dotted underline-offset-[3px] transition-colors duration-200 hover:text-main-500/90"
+    {...props}
+  >
+    {children}
+  </Link>
+)
+
 export default function AboutPage() {
   return (
     <div className="py-10 md:py-14">
       <div className="px-2 md:px-20">
-        <h2 className="mb-2 text-lg font-bold md:text-3xl">How it works</h2>
+        <SectionTitle>How it works</SectionTitle>
         <p className="py-2">
           This project uses the GitHub GraphQL API to fetch data and uses Next.js API Routes to send
           requests, you can read{' '}
@@ -21,18 +39,36 @@ export default function AboutPage() {
           to learn how we handle your data.
         </p>
 
-        <h2 className="mt-8 mb-2 text-lg font-bold md:text-3xl">Credits</h2>
+        <SectionTitle>Credits</SectionTitle>
+        <ul className="list-inside list-disc py-2 pl-1 marker:text-sm marker:text-main-700/90">
+          <li>
+            <i className="mr-3 opacity-90">Inspired by:</i>
+            <TextLink
+              passHref
+              href="https://github.com/sallar/github-contributions-chart"
+              target="_blank"
+            >
+              GitHub Contributions Chart Generator.
+            </TextLink>
+          </li>
+          <li>
+            <i className="mr-3 opacity-90">Framework:</i>Next.js.
+          </li>
+          <li>
+            <i className="mr-3 opacity-90">Font:</i>
+            <TextLink passHref href="https://fonts.google.com/specimen/Rubik" target="_blank">
+              Rubik
+            </TextLink>{' '}
+            by Google Fonts.
+          </li>
+        </ul>
+
+        <SectionTitle>Reporting issues</SectionTitle>
         <p className="py-2">
-          Thanks to the{' '}
-          <Link
-            passHref
-            className="underline decoration-current decoration-dotted underline-offset-[3px] transition-colors duration-200 hover:text-main-500/90"
-            href="https://github.com/sallar/github-contributions-chart"
-            target="_blank"
-          >
-            &ldquo;GitHub Contributions Chart Generator&rdquo;
-          </Link>{' '}
-          for the inspiration, it&apos;s a very interesting project !
+          Got bugs or have any suggestions for this project? Welcome to leave your message on the{' '}
+          <TextLink passHref href="https://github.com/Codennnn/Green-Wall/issues" target="_blank">
+            GitHub issues.
+          </TextLink>
         </p>
       </div>
     </div>
