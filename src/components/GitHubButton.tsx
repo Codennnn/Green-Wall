baf29d8197ce77fbe98f7ca080cc90abbf59a98b
LeoKu <czc12580520@gmail.com>
2022-12-30 08:57:15 +0800

    Update button text

--- src/components/GitHubButton.tsx
+++ src/components/GitHubButton.tsx
@@ -21,7 +21,7 @@ export default function GitHubButton() {
     >
       <button className="flex items-center rounded-md bg-main-100 px-3 py-2 text-sm font-medium text-main-500 ring-4 ring-white transition-colors duration-300 hover:bg-main-200 md:ring-8">
         {iconGitHub}
-        <span className="ml-2">Source Code</span>
+        <span className="ml-2">Open Source</span>
       </button>
     </Link>
   )
