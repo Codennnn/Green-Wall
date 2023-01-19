22044f7eec175b1cf99374c84239eb5a61d759cf
LeoKu <czc12580520@gmail.com>
2023-01-19 10:06:23 +0800

    Simplified tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ const plugin = require('tailwindcss/plugin')
 
 /** @type {import('tailwindcss').Config} \*/
 module.exports = {
-  content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/{pages,components}/**/*.{js,jsx,ts,tsx}'],
 
   theme: {
     extend: {
