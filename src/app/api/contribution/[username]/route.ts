4ce5a09065f2b877effbcfd179c933943842013b
LeoKu <leokudev@gmail.com>
2023-08-25 13:08:33 +0800

    Migrate to `App Router`

--- src/app/api/contribution/[username]/route.ts
+++ src/app/api/contribution/[username]/route.ts
@@ -1,4 +1,4 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { type NextRequest, NextResponse } from 'next/server'
 
 import {
   type ContributionBasic,
@@ -114,8 +114,8 @@ async function fetchContributionsCollection(
   return { ...contributionCalendar, year }
 }
 
-export default async function (req: NextApiRequest, res: NextApiResponse<ResponseData>) {
-  const { username } = req.query
+export async function GET(_: NextRequest, { params }: { params: { username: string } }) {
+  const { username } = params
 
   if (typeof username === 'string') {
     try {
@@ -127,15 +127,18 @@ export default async function (req: NextApiRequest, res: NextApiResponse<Respons
 
       const data: GraphData = { ...githubUser, contributionCalendars }
 
-      return res.status(200).json({ data })
+      return NextResponse.json({ data }, { status: 200 })
     } catch (e: any) {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
       const errorData: ResponseData = { errorType: ErrorType.BadRequest, message: e.message }
       // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
       if (e.message === 'Bad credentials') {
-        return res.status(401).json({ ...errorData, errorType: ErrorType.BadCredentials })
+        return NextResponse.json(
+          { ...errorData, errorType: ErrorType.BadCredentials },
+          { status: 401 }
+        )
       }
-      return res.status(400).json(errorData)
+      return NextResponse.json(errorData, { status: 400 })
     }
   }
 }
