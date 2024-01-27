d1ba42ec6d6beb745702be1f4acccf1fe8c798e0
LeoKu <leokudev@gmail.com>
2024-01-27 16:37:39 +0800

    Format code

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,10 +2,13 @@ const { TYPESCRIPT_FILES } = require('prefer-code-style/constants')
 
 module.exports = {
   root: true,
+
   extends: [require.resolve('prefer-code-style/eslint/preset/next')],
+
   rules: {
     'import/no-unresolved': [2, { ignore: ['^\\~/'] }],
   },
+
   overrides: [
     {
       files: TYPESCRIPT_FILES,
