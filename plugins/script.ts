ef27033cb1abf8bcbbb6663d26aea7e9c2559541
LeoKu <leokudev@gmail.com>
2024-04-19 22:30:42 +0800

    Update Tampermonkey script

--- plugins/script.ts
+++ plugins/script.ts
@@ -1,4 +1,170 @@
-function createDialog(): { dialog: HTMLDialogElement } {
+const ORIGIN = 'https://green-wall.leoku.dev'
+
+const enum ContributionLevel {
+  Null = 'Null',
+  NONE = 'NONE',
+  FIRST_QUARTILE = 'FIRST_QUARTILE',
+  SECOND_QUARTILE = 'SECOND_QUARTILE',
+  THIRD_QUARTILE = 'THIRD_QUARTILE',
+  FOURTH_QUARTILE = 'FOURTH_QUARTILE',
+}
+
+interface ContributionBasic {
+  name?: string
+  login: string
+  avatarUrl: string
+  contributionYears: number[]
+  contributionCalendars: {
+    total: number
+    year: number
+    weeks: {
+      days: {
+        level: `${ContributionLevel}`
+        weekday?: 0 | 1 | 2 | 3 | 4 | 5 | 6
+      }[]
+    }[]
+  }[]
+}
+
+interface Data {
+  data: ContributionBasic
+}
+
+type Weekday = 0 | 1 | 2 | 3 | 4 | 5 | 6
+
+interface Calendar {
+  total: number
+  year: number
+  rows: {
+    level: `${ContributionLevel}`
+    weekday?: Weekday
+  }[][]
+}
+
+interface ProducedData {
+  contributionCalendars: Calendar[]
+}
+
+function produceData({ data }: Data): ProducedData {
+  const contributionCalendars = data.contributionCalendars.map<Calendar>((cur) => {
+    const rows: Calendar['rows'] = [[], [], [], [], [], [], []]
+
+    cur.weeks.forEach(({ days }) => {
+      if (days.length !== 7) {
+        const newDays = [...days]
+
+        for (let i = 0; i <= 6; i++) {
+          const theDay = newDays.at(i)
+
+          const weekday = i as Weekday
+
+          if (theDay && typeof theDay.weekday === 'number') {
+            if (theDay.weekday === weekday) {
+              rows[theDay.weekday].push(theDay)
+            } else {
+              newDays.splice(i, 0, { level: 'Null', weekday })
+              rows[i].push({ level: 'Null', weekday })
+            }
+          } else {
+            rows[i].push({ level: 'Null', weekday })
+          }
+        }
+      } else {
+        days.forEach((day) => {
+          if (typeof day.weekday === 'number') {
+            rows[day.weekday].push(day)
+          }
+        })
+      }
+    })
+
+    const calendar = {
+      total: cur.total,
+      year: cur.year,
+      rows,
+    }
+
+    return calendar
+  })
+
+  return {
+    contributionCalendars,
+  }
+}
+
+function createGraph(params: Calendar): {
+  graphItem: HTMLDivElement
+} {
+  const { year, total, rows } = params
+
+  const table = document.createElement('table')
+  table.classList.add('ContributionCalendar-grid')
+  table.style.borderSpacing = '3px'
+  table.style.overflow = 'hidden'
+  table.style.position = 'relative'
+
+  const tbody = document.createElement('tbody')
+  const tr = document.createElement('tr')
+  tr.style.height = '10px'
+
+  rows.forEach((row) => {
+    const clonedTr = tr.cloneNode()
+
+    let htmlStr = ''
+
+    row.forEach((col, idx) => {
+      let td = '<td></td>'
+
+      if (col.level !== ContributionLevel.Null) {
+        const level =
+          col.level === ContributionLevel.NONE
+            ? 0
+            : col.level === ContributionLevel.FIRST_QUARTILE
+              ? 1
+              : col.level === ContributionLevel.SECOND_QUARTILE
+                ? 2
+                : col.level === ContributionLevel.THIRD_QUARTILE
+                  ? 3
+                  : 4
+
+        td = `
+        <td
+          tabindex="-1"
+          data-ix="${idx}"
+          style="width: 10px"
+          data-level="${level}"
+          role="gridcell"
+          class="ContributionCalendar-day"
+        ></td>
+        `
+      }
+
+      htmlStr += td
+    })
+
+    if (clonedTr instanceof HTMLTableRowElement) {
+      clonedTr.innerHTML = htmlStr
+      tbody.append(clonedTr)
+    }
+  })
+
+  table.appendChild(tbody)
+
+  const graphItem = document.createElement('div')
+  const countText = document.createElement('div')
+  countText.style.marginBottom = '5px'
+  countText.textContent = `${total} contributions in ${year}`
+  graphItem.append(countText, table)
+
+  return { graphItem }
+}
+
+function createDialog(params: { username: string }): {
+  dialog: HTMLDialogElement
+  dialogContent: HTMLDivElement
+} {
+  const { username } = params
+
   const dialog = document.createElement('dialog')
   dialog.id = 'green-wall-dialog'
   dialog.classList.add(
@@ -10,6 +176,29 @@ function createDialog(): { dialog: HTMLDialogElement } {
     'Overlay--size-medium-portrait',
     'Overlay--motion-scaleFade'
   )
+  dialog.style.minWidth = '720px'
+  dialog.style.maxHeight = 'calc(100vh - 50px)'
+
+  const handleDialogClose = () => {
+    dialog.close()
+    document.body.classList.remove('has-modal')
+  }
+
+  dialog.addEventListener('click', () => {
+    handleDialogClose()
+  })
+
+  // ---
+
+  const wrap = document.createElement('div')
+  wrap.style.display = 'flex'
+  wrap.style.flexDirection = 'column'
+  wrap.style.overflow = 'hidden'
+  wrap.addEventListener('click', (ev) => {
+    ev.stopPropagation()
+  })
+
+  // ---
 
   const dialogHeader = document.createElement('div')
   dialogHeader.classList.add('Overlay-header')
@@ -22,7 +211,7 @@ function createDialog(): { dialog: HTMLDialogElement } {
 
   const title = document.createElement('h1')
   title.classList.add('Overlay-title')
-  title.textContent = 'All Green'
+  title.textContent = `${username}'s GreenWall`
 
   const actionWrap = document.createElement('div')
   actionWrap.classList.add('Overlay-actionWrap')
@@ -35,29 +224,62 @@ function createDialog(): { dialog: HTMLDialogElement } {
     <path d="M3.72 3.72a.75.75 0 0 1 1.06 0L8 6.94l3.22-3.22a.749.749 0 0 1 1.275.326.749.749 0 0 1-.215.734L9.06 8l3.22 3.22a.749.749 0 0 1-.326 1.275.749.749 0 0 1-.734-.215L8 9.06l-3.22 3.22a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042L6.94 8 3.72 4.78a.75.75 0 0 1 0-1.06Z"></path>
   </svg>
   `
-  actionButton.addEventListener('click', () => {
-    dialog.close()
+  actionButton.addEventListener('click', (ev) => {
+    ev.stopPropagation()
+    handleDialogClose()
   })
+
+  // ---
 
   const dialogBody = document.createElement('div')
   dialogBody.classList.add('Overlay-body')
+  dialogBody.style.overflowY = 'auto'
+
+  const dialogContent = document.createElement('div')
+  dialogContent.style.display = 'flex'
+  dialogContent.style.flexDirection = 'column'
+  dialogContent.style.rowGap = '10px'
+  dialogContent.style.alignItems = 'center'
+  dialogContent.style.padding = 'var(--stack-padding-normal, 1rem)'
+
+  // ---
+
+  const dialogFooter = document.createElement('div')
+  dialogFooter.classList.add(
+    'Overlay-footer',
+    'Overlay-footer--alignEnd',
+    'Overlay-footer--divided'
+  )
+  const openExtrnalBtn = document.createElement('button')
+  const btnContent = document.createElement('span')
+  btnContent.classList.add('Button-label')
+  btnContent.textContent = 'Open in Green Wall'
+  openExtrnalBtn.classList.add('Button', 'Button--primary', 'Button--medium')
+  openExtrnalBtn.addEventListener('click', () => {
+    window.open(`${ORIGIN}/share/${username}`, '_blank')
+  })
 
   titleWrap.append(title)
   actionWrap.append(actionButton)
-  contentWrap.append(titleWrap)
-  contentWrap.append(actionWrap)
+  contentWrap.append(titleWrap, actionWrap)
+  openExtrnalBtn.append(btnContent)
   dialogHeader.append(contentWrap)
-  dialog.append(dialogHeader)
-  dialog.append(dialogBody)
+  dialogBody.append(dialogContent)
+  dialogFooter.append(openExtrnalBtn)
+
+  wrap.append(dialogHeader, dialogBody, dialogFooter)
+  dialog.append(wrap)
+
   document.body.append(dialog)
 
-  return { dialog }
+  return { dialog, dialogContent }
 }
 
-const attachTarget = document.querySelector('.Layout-sidebar .h-card .js-profile-editable-replace')
-const refNode = document.getElementsByClassName('border-top color-border-muted')[0]
+const profileArea = document.querySelector('.Layout-sidebar .h-card .js-profile-editable-replace')
+const refNode = document.querySelector('.js-profile-editable-replace > .d-flex.flex-column')
+  ?.nextSibling?.nextSibling
 
-if (attachTarget instanceof HTMLElement) {
+if (profileArea instanceof HTMLElement && refNode instanceof HTMLElement) {
   const username = document
     .querySelector('meta[property="profile:username"]')
     ?.getAttribute('content')
@@ -78,20 +300,92 @@ if (attachTarget instanceof HTMLElement) {
     title.classList.add('h4', 'mb-2')
     title.textContent = 'Green Wall'
 
-    const button = document.createElement('button')
-    button.classList.add('btn')
-    button.textContent = 'View All Green'
+    const openBtn = document.createElement('button')
+    openBtn.classList.add('btn')
+    openBtn.textContent = 'View All Green'
 
     block.appendChild(title)
-    block.appendChild(button)
+    block.appendChild(openBtn)
 
-    attachTarget.insertBefore(block, refNode)
+    profileArea.insertBefore(block, refNode)
 
-    const { dialog } = createDialog()
+    const { dialog, dialogContent } = createDialog({ username })
 
-    button.addEventListener('click', () => {
-      // window.open(`https://green-wall.leoku.dev/share/${username}`, '_blank');
+    let hasLoaded = false // 标志变量，用来判断是否已经加载过内容。
+
+    const handleLoadError = () => {
+      dialogContent.innerHTML = ''
+
+      const errorBlock = document.createElement('div')
+      errorBlock.style.display = 'flex'
+      errorBlock.style.flexDirection = 'column'
+      errorBlock.style.alignItems = 'center'
+
+      const tip = document.createElement('p')
+      tip.textContent = '获取数据的过程出现异常。'
+
+      const retryBtn = document.createElement('button')
+      retryBtn.classList.add('btn')
+      retryBtn.textContent = '点击重试'
+      retryBtn.addEventListener('click', () => {
+        // eslint-disable-next-line @typescript-eslint/no-use-before-define
+        handleLoadData()
+      })
+
+      errorBlock.append(tip, retryBtn)
+
+      dialogContent.append(errorBlock)
+    }
+
+    const handleLoadData = () => {
+      const loading = `
+        <svg aria-label="Loading" style="box-sizing: content-box; color: var(--color-icon-primary);" width="32" height="32" viewBox="0 0 16 16" fill="none" data-view-component="true" class="anim-rotate">
+          <circle cx="8" cy="8" r="7" stroke="currentColor" stroke-opacity="0.25" stroke-width="2" vector-effect="non-scaling-stroke" fill="none"></circle>
+          <path d="M15 8a7.002 7.002 0 00-7-7" stroke="currentColor" stroke-width="2" stroke-linecap="round" vector-effect="non-scaling-stroke"></path>
+        </svg>
+        `
+      dialogContent.innerHTML = loading
+
+      GM.xmlHttpRequest({
+        method: 'GET',
+        url: `${ORIGIN}/api/contribution/${username}`,
+        onload: (response) => {
+          try {
+            dialogContent.innerHTML = ''
+
+            const data: Data = JSON.parse(response.responseText)
+            const xData = produceData(data)
+
+            xData.contributionCalendars.forEach((calendar) => {
+              const { graphItem } = createGraph(calendar)
+              dialogContent.append(graphItem)
+            })
+
+            hasLoaded = true
+          } catch {
+            handleLoadError()
+          }
+        },
+        onerror: (err) => {
+          console.error('[Green Wall]: ', err)
+          handleLoadError()
+        },
+      })
+    }
+
+    const handleDialogOpen = () => {
       dialog.showModal()
+      document.body.classList.add('has-modal')
+
+      if (!hasLoaded) {
+        handleLoadData()
+      }
+    }
+
+    openBtn.addEventListener('click', () => {
+      handleDialogOpen()
     })
   }
+} else {
+  console.warn('[Green Wall]: 未找到目标节点。')
 }
