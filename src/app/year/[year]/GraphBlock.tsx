332e985c29d873ed18470e394553d5bd8d0d434c
LeoKu <leokudev@gmail.com>
2024-11-07 13:51:27 +0800

    feat: add spinngin animation

--- src/app/year/[year]/GraphBlock.tsx
+++ src/app/year/[year]/GraphBlock.tsx
@@ -1,17 +1,44 @@
 import { useEffect, useState } from 'react'
 
 import { useParams, useSearchParams } from 'next/navigation'
+import { LoaderIcon } from 'lucide-react'
+import { array, number, object, parse, string } from 'valibot'
 
 import { ContributionsGraph } from '~/components/ContributionsGraph'
 import Loading from '~/components/Loading'
 import { useData } from '~/DataContext'
 import { getLongestContributionStreak, getMaxContributionsInADay } from '~/helpers'
-import { mockGraphData } from '~/mock-data'
-import { getIssuesInYear, getReposCreatedInYear } from '~/services'
 import type { IssuesInYear, RepoCreatedInYear } from '~/types'
 import { useGraphRequest } from '~/useGraphRequest'
 
+const ReposSchema = object({
+  count: number(),
+  repos: array(
+    object({
+      name: string(),
+      createdAt: string(),
+    })
+  ),
+})
+
+const IssuesSchema = object({
+  count: number(),
+  issues: array(
+    object({
+      title: string(),
+      createdAt: string(),
+      repository: object({
+        nameWithOwner: string(),
+      }),
+    })
+  ),
+})
+
 type StaticCardProps = React.PropsWithChildren<Pick<React.ComponentProps<'div'>, 'className'>>
+
+function SpinningLoader() {
+  return <LoaderIcon className="size-4 animate-spin text-main-500" />
+}
 
 function StaticCard(props: StaticCardProps) {
   const { children, className = '' } = props
@@ -44,23 +71,29 @@ export function GraphBlock() {
   const [maxContributionStreak, setMaxContributionStreak] = useState<number>()
   const [maxContributionsInADay, setMaxContributionsInADay] = useState<number>()
 
+  const [loadingRepos, setLoadingRepos] = useState(true)
+  const [loadingIssues, setLoadingIssues] = useState(true)
   const [reposCreatedInYear, setReposCreatedInYear] = useState<RepoCreatedInYear>()
   const [issuesInYear, setIssuesInYear] = useState<IssuesInYear>()
 
-  const { graphData, setGraphData } = useData()
+  const { setGraphData } = useData()
 
   useEffect(() => {
     if (githubUsername) {
       void (async () => {
         fetch(`/api/repos?username=${githubUsername}&year=${queryYear}`).then(async (res) => {
           if (res.ok) {
-            setReposCreatedInYear(await res.json())
+            const repos = parse(ReposSchema, await res.json())
+            setReposCreatedInYear(repos)
+            setLoadingRepos(false)
           }
         })
 
         fetch(`/api/issues?username=${githubUsername}&year=${queryYear}`).then(async (res) => {
           if (res.ok) {
-            setIssuesInYear(await res.json())
+            const issues = parse(IssuesSchema, await res.json())
+            setIssuesInYear(issues)
+            setLoadingIssues(false)
           }
         })
 
@@ -84,31 +117,45 @@ export function GraphBlock() {
       <Loading active={loading}>
         <ContributionsGraph showInspect={false} />
 
-        {!!graphData && (
-          <>
-            <div className="grid grid-cols-2 gap-3">
-              <StaticCard>
-                <span className="font-semibold">Longest Streak</span>
-                <span className="ml-auto">{maxContributionStreak}</span>
-              </StaticCard>
+        <>
+          <div className="grid grid-cols-2 gap-3">
+            <StaticCard>
+              <span className="font-semibold">Longest Streak</span>
+              <span className="ml-auto">
+                {typeof maxContributionStreak === 'number' ? (
+                  maxContributionStreak
+                ) : (
+                  <SpinningLoader />
+                )}
+              </span>
+            </StaticCard>
 
-              <StaticCard>
-                <span className="font-semibold">Max Contributions in a Day</span>
-                <span className="ml-auto">{maxContributionsInADay}</span>
-              </StaticCard>
+            <StaticCard>
+              <span className="font-semibold">Max Contributions in a Day</span>
+              <span className="ml-auto">
+                {typeof maxContributionsInADay === 'number' ? (
+                  maxContributionsInADay
+                ) : (
+                  <SpinningLoader />
+                )}
+              </span>
+            </StaticCard>
 
-              <StaticCard>
-                <span className="font-semibold">Repos Created in {queryYear}</span>
-                <span className="ml-auto">{reposCreatedInYear?.count}</span>
-              </StaticCard>
+            <StaticCard>
+              <span className="font-semibold">Repos Created in {queryYear}</span>
+              <span className="ml-auto">
+                {loadingRepos ? <SpinningLoader /> : reposCreatedInYear?.count}
+              </span>
+            </StaticCard>
 
-              <StaticCard>
-                <span className="font-semibold">Issues in {queryYear}</span>
-                <span className="ml-auto">{issuesInYear?.count}</span>
-              </StaticCard>
-            </div>
-          </>
-        )}
+            <StaticCard>
+              <span className="font-semibold">Issues in {queryYear}</span>
+              <span className="ml-auto">
+                {loadingIssues ? <SpinningLoader /> : issuesInYear?.count}
+              </span>
+            </StaticCard>
+          </div>
+        </>
       </Loading>
     </div>
   )
